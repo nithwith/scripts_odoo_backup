480e664a3f7b2795bce11c07d8259fd1c6c66585
thm <thm@apik.cloud>
2025-03-22 13:35:39 +0100

    [IMP] Little improvement

--- make_odoo_backup.py
+++ make_odoo_backup.py
@@ -1,8 +1,9 @@
 import xmlrpc.client
 import os, time, logging, subprocess
-from dotenv import load_dotenv
+from dotenv.main import load_dotenv
 
 load_dotenv()
+
 ODOO_URL = os.getenv('ODOO_URL')
 ODOO_DB = os.getenv('ODOO_DB')
 ODOO_USERNAME = os.getenv('ODOO_USERNAME')
